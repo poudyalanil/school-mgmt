1e1fbe2fdcd19c8a90c4bd8482a1eaba9f11d571
Anil Poudyal <anilrunjakote@gmail.com>
2020-07-01 00:07:00 +0545

    Admin Side

--- main.py
+++ main.py
@@ -22,10 +22,49 @@ def gallery():
 def contact():
     return "Contact Page"
 
+##############################
+##############################
+######### Admin Section ########
+
 
 @app.route("/admin/")
-def admin():
-    return "Admin Page"
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+
+@app.route("/admin/application/")
+def admin_application():
+    return render_template('admin_applications.html')
+
+
+@app.route("/admin/calendar/")
+def admin_calendar():
+    return render_template('admin_calendar.html')
+
+
+@app.route("/admin/gallery/")
+def admin_gallery():
+    return render_template('admin_gallery.html')
+
+
+@app.route("/admin/media/")
+def admin_media():
+    return render_template('admin_media.html')
+
+
+@app.route("/admin/notice/")
+def admin_notice():
+    return render_template('admin_notice.html')
+
+
+@app.route("/admin/profile/")
+def admin_profile():
+    return render_template('admin_profile.html')
+
+
+@app.route("/admin/users/")
+def admin_users():
+    return render_template('admin_users.html')
 
 
 if __name__ == "__main__":
